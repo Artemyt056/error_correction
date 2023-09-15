8764a94dc8d56d2ff79ead2cc913a09b173fcb40
Artemyt <artemyt056@gmail.com>
2023-09-15 14:05:09 +0300

    error_correction

---
+++ error_correction.py
@@ -0,0 +1,15 @@
+# expected result in the form: My name is David, I am 14 years old👣
+
+# example
+
+# f = '\N{Footprints}' not informative naming, the correct code below
+
+smile_footprint = '\U0001F463'
+
+User_name = 'Max'
+
+User_age = 'five'
+
+result = f'My name is {User_name} I am {User_age}  years old {smile_footprint}'
+
+print(result)
